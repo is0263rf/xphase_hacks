317ea22b6b03cf7568fc9655ffd27932f89e27e4
Andy Dodd <atd7@cornell.edu>
2024-04-03 17:31:17 -0400

    xphase_data:  Rework completely
    Remove all of the guesswork over transfer functions, and instead use transfer functions and matrices obtained
    via reverse engineering PanoMakerPro.dll using Ghidra

--- xphase_data.py
+++ xphase_data.py
@@ -2,85 +2,65 @@
 
 import numpy as np
 
-class XphaseTransfer:
-    def __init__(self, c=3800):
+class XphaseTransforms:
+    def __init__(self):
 
-        # This was derived by feeding a white gradient (e.g. luminance only) to PanoManager
-        # It's extremely accurate in the 23-229 range, but that doesn't work with
-        # xphase's failed attempt to be clever and encode out-of-gamut colors with
-        # negative values
-        self.lut = np.array([1.0000e+00, 1.0000e+00, 1.0000e+00, 1.0000e+00, 1.0000e+00,
-            1.0000e+00, 1.0000e+00, 1.0000e+00, 1.0000e+00, 1.0000e+00,
-            1.0000e+00, 1.0000e+00, 1.0000e+00, 1.0000e+00, 1.0000e+00,
-            1.0000e+00, 1.0000e+00, 1.0000e+00, 1.0000e+00, 1.0000e+00,
-            1.0000e+00, 1.0000e+00, 1.0000e+00, 1.0000e+00, 4.1000e+01,
-            8.7000e+01, 1.3400e+02, 1.8100e+02, 2.5100e+02, 2.9800e+02,
-            3.4500e+02, 3.9100e+02, 4.6100e+02, 5.0800e+02, 5.5500e+02,
-            6.2500e+02, 6.7200e+02, 7.4200e+02, 7.8900e+02, 8.5900e+02,
-            9.0600e+02, 9.7600e+02, 1.0460e+03, 1.1160e+03, 1.1630e+03,
-            1.2330e+03, 1.3030e+03, 1.3740e+03, 1.4440e+03, 1.5140e+03,
-            1.5840e+03, 1.6540e+03, 1.7240e+03, 1.8180e+03, 1.8880e+03,
-            1.9580e+03, 2.0520e+03, 2.1220e+03, 2.2160e+03, 2.2860e+03,
-            2.3790e+03, 2.4730e+03, 2.5430e+03, 2.6370e+03, 2.7300e+03,
-            2.8240e+03, 2.9170e+03, 3.0110e+03, 3.1040e+03, 3.1980e+03,
-            3.3150e+03, 3.4080e+03, 3.5020e+03, 3.6190e+03, 3.7120e+03,
-            3.8290e+03, 3.9230e+03, 4.0400e+03, 4.1570e+03, 4.2740e+03,
-            4.3910e+03, 4.5080e+03, 4.6240e+03, 4.7410e+03, 4.8580e+03,
-            4.9990e+03, 5.1160e+03, 5.2560e+03, 5.3730e+03, 5.5130e+03,
-            5.6540e+03, 5.7700e+03, 5.9110e+03, 6.0750e+03, 6.2150e+03,
-            6.3550e+03, 6.4960e+03, 6.6360e+03, 6.8000e+03, 6.9630e+03,
-            7.1040e+03, 7.2670e+03, 7.4310e+03, 7.5950e+03, 7.7580e+03,
-            7.9460e+03, 8.1090e+03, 8.2730e+03, 8.4600e+03, 8.6470e+03,
-            8.8110e+03, 8.9980e+03, 9.1850e+03, 9.3720e+03, 9.5590e+03,
-            9.7700e+03, 9.9800e+03, 1.0167e+04, 1.0378e+04, 1.0588e+04,
-            1.0799e+04, 1.1009e+04, 1.1243e+04, 1.1454e+04, 1.1664e+04,
-            1.1898e+04, 1.2132e+04, 1.2366e+04, 1.2600e+04, 1.2857e+04,
-            1.3091e+04, 1.3348e+04, 1.3582e+04, 1.3839e+04, 1.4097e+04,
-            1.4377e+04, 1.4634e+04, 1.4915e+04, 1.5172e+04, 1.5453e+04,
-            1.5734e+04, 1.6014e+04, 1.6318e+04, 1.6599e+04, 1.6903e+04,
-            1.7207e+04, 1.7511e+04, 1.7839e+04, 1.8143e+04, 1.8470e+04,
-            1.8797e+04, 1.9125e+04, 1.9452e+04, 1.9803e+04, 2.0154e+04,
-            2.0481e+04, 2.0832e+04, 2.1206e+04, 2.1557e+04, 2.1931e+04,
-            2.2306e+04, 2.2680e+04, 2.3077e+04, 2.3452e+04, 2.3849e+04,
-            2.4247e+04, 2.4668e+04, 2.5065e+04, 2.5486e+04, 2.5907e+04,
-            2.6352e+04, 2.6796e+04, 2.7217e+04, 2.7685e+04, 2.8129e+04,
-            2.8573e+04, 2.9041e+04, 2.9532e+04, 3.0000e+04, 3.0515e+04,
-            3.0982e+04, 3.1474e+04, 3.2011e+04, 3.2503e+04, 3.3017e+04,
-            3.3555e+04, 3.4070e+04, 3.4631e+04, 3.5169e+04, 3.5754e+04,
-            3.6291e+04, 3.6876e+04, 3.7461e+04, 3.8022e+04, 3.8630e+04,
-            3.9238e+04, 3.9846e+04, 4.0478e+04, 4.1109e+04, 4.1717e+04,
-            4.2396e+04, 4.3027e+04, 4.3682e+04, 4.4384e+04, 4.5038e+04,
-            4.5740e+04, 4.6442e+04, 4.7143e+04, 4.7845e+04, 4.8593e+04,
-            4.9342e+04, 5.0043e+04, 5.0839e+04, 5.1587e+04, 5.2359e+04,
-            5.3154e+04, 5.3949e+04, 5.4744e+04, 5.5563e+04, 5.6405e+04,
-            5.7223e+04, 5.8065e+04, 5.8931e+04, 5.9843e+04, 6.0685e+04,
-            6.1597e+04, 6.2509e+04, 6.3421e+04, 6.4333e+04, 6.5281e+04,
-            6.5281e+04, 6.5281e+04, 6.5281e+04, 6.5281e+04, 6.5281e+04,
-            6.5281e+04, 6.5281e+04, 6.5281e+04, 6.5281e+04, 6.5281e+04,
-            6.5281e+04, 6.5281e+04, 6.5281e+04, 6.5281e+04, 6.5281e+04,
-            6.5281e+04, 6.5281e+04, 6.5281e+04, 6.5281e+04, 6.5281e+04,
-            6.5281e+04, 6.5281e+04, 6.5281e+04, 6.5281e+04, 6.5281e+04,
-            6.5281e+04])
+        # Pulled from PanoManager's PanoMakerPro.dll using Ghidra
+        # This is in theory a close match to the camera's internal sensor->JPEG LUT, except only has
+        # 256 entries for a 10 bit sensor...  Hisilicon limitation???
+        self.lut = np.array([   0,  172,  318,  444,  556,  656,  747,  830,  907,  978, 1045,
+       1107, 1166, 1222, 1275, 1325, 1373, 1419, 1463, 1505, 1546, 1585,
+       1623, 1659, 1694, 1728, 1762, 1794, 1825, 1855, 1884, 1913, 1941,
+       1968, 1995, 2021, 2046, 2071, 2095, 2119, 2142, 2165, 2187, 2209,
+       2231, 2252, 2272, 2293, 2313, 2332, 2351, 2370, 2389, 2407, 2425,
+       2443, 2460, 2478, 2495, 2511, 2528, 2544, 2560, 2576, 2591, 2607,
+       2622, 2637, 2652, 2666, 2681, 2695, 2709, 2723, 2736, 2750, 2763,
+       2777, 2790, 2803, 2816, 2828, 2841, 2853, 2865, 2878, 2890, 2902,
+       2913, 2925, 2937, 2948, 2959, 2971, 2982, 2993, 3004, 3014, 3025,
+       3036, 3046, 3057, 3067, 3077, 3088, 3098, 3108, 3118, 3127, 3137,
+       3147, 3156, 3166, 3175, 3185, 3194, 3203, 3212, 3222, 3231, 3240,
+       3248, 3257, 3266, 3275, 3283, 3292, 3300, 3309, 3317, 3326, 3334,
+       3342, 3350, 3358, 3367, 3375, 3382, 3390, 3398, 3406, 3414, 3422,
+       3429, 3437, 3444, 3452, 3459, 3467, 3474, 3482, 3489, 3496, 3503,
+       3510, 3518, 3525, 3532, 3539, 3546, 3553, 3559, 3566, 3573, 3580,
+       3587, 3593, 3600, 3607, 3613, 3620, 3626, 3633, 3639, 3646, 3652,
+       3658, 3665, 3671, 3677, 3684, 3690, 3696, 3702, 3708, 3714, 3720,
+       3726, 3732, 3738, 3744, 3750, 3756, 3762, 3768, 3773, 3779, 3785,
+       3791, 3796, 3802, 3808, 3813, 3819, 3824, 3830, 3835, 3841, 3846,
+       3852, 3857, 3863, 3868, 3873, 3879, 3884, 3889, 3895, 3900, 3905,
+       3910, 3915, 3921, 3926, 3931, 3936, 3941, 3946, 3951, 3956, 3961,
+       3966, 3971, 3976, 3981, 3986, 3991, 3996, 4000, 4005, 4010, 4015,
+       4020, 4024, 4029, 4034, 4038, 4043, 4048, 4052, 4057, 4062, 4066,
+       4071, 4075, 4080], dtype=np.uint16)
 
-        self.ilut = np.digitize(np.arange(0.0,65282.0,1.0),self.lut,right=True)
+        # Rather than store the JPEG-to-RAW LUT directly, PanoManager interpolates from the above LUT
+        # Experimentation verified that this provides identical results to PanoManager's LUT without
+        # the padded 0xff0 in entry 257 which will never be used.  Xphase loves off-by-one errors in
+        # their software...
+        self.ilut = np.round(np.interp(np.arange(256)*16, self.lut, np.arange(256)*16)).astype(np.int32)
+        
+        # Also pulled from PanoMakerPro.dll using Ghidra
+        self.jpeg_to_raw_matrix = np.array([17666,  3471,  2812,  4951, 14813,  4573,  2441,  3832, 17689], dtype=np.int16).reshape((3,3))
 
-        # 3800 is a very close fit of the transfer function to the LUT in the 23-229 range
-        # but it isn't exact...  Xphase refuses to document their transfer function
-        self.c = c
 
-    # These derive a and b based on the following assumptions:
-    # linearizing 65281 always results in 229
-    # linearizing 1 always results in 23
-    def linearize_code(self,code):
-        c = self.c
-        a = (np.log2(c+1) - np.log2(c + 65281))/-206
-        b = -np.log2(c+1) + 23*a
-
-        return np.power(2,(code*a-b))-c
-
-    def code_from_linear(self, lin):
-        c = self.c
-        a = (np.log2(c+1) - np.log2(c + 65281))/-206
-        b = -np.log2(c+1) + 23*a
-
-        return (np.log2(lin+c)+b)/a
+    def jpeg_to_raw(self, imagedata):
+        # Invert Xphase's poorly designed JPEG pipeline
+        # After applying a fixed D50 white balance to the raw sensor data (even out-of-camera DNGs have this...), the camera adds a black level offset for unknown reasons -
+        # the only possible explanation is to desaturate darks to allow representing a wider gamut in the shadows but HDR merging will throw out the shadows of eveery JPEG,
+        # except the darkest - so the gamut won't be expanded at any usable luminance, and this approach reduces usable luminance range.
+        #
+        # It then compresses 10 bits down to 8 using a variation of the LUT stored at the beginning of this file.  Afterwards, it encodes a YUV420 JPEG
+        # using the standard JPEG RGB->YUV conversion matrix.
+        #
+        # The below inverse LUT followed by a matrix transformation then subtraction undoes this pipeline, and exactly matches PanoManager's conversion pipeline
+        # Note the + 0x200 offset - this appears to be designed to convert a floor/truncation to rounding by adding a value that corresponds to 0.5 in floating point
+        #, as 0x400 translates to 1 when right shifted by 10 bits.
+        h = imagedata.shape[0]
+        w = imagedata.shape[1]
+        imagedata = imagedata.reshape((w*h, 3))
+        rawdata = ((np.matmul(self.jpeg_to_raw_matrix, self.ilut[imagedata.T]).T + 0x200) >> 10) - 0x380
+        rawdata = rawdata.reshape((h, w, 3))
+        rawdata = np.clip(rawdata, 0, 0xff00).astype(np.uint16)
+        # Xphase adds 1 to everything on export.  WHY?  This will cause a black level offset in the shadows
+        # FIXME:  Make this behavior an option
+        return rawdata + 1
