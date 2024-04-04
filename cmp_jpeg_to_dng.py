cbbb4ae86e0f9c5b60542b4001515d9166d8bf6e
Andy Dodd <atd7@cornell.edu>
2024-04-04 12:50:33 -0400

    DNG/JPEG compare tool:
    Add Mode 3 to compare a PanoManager DNG to one generated using the transforms pulled using Ghidra
    
    Modes 0/1/2 are probably broken due to the massive rework of xphase_data, but honestly these really
    no longer have a purpose.

--- cmp_jpeg_to_dng.py
+++ cmp_jpeg_to_dng.py
@@ -9,13 +9,16 @@ import argparse
 import os
 import colour
 from pprint import pprint
-from xphase_data import XphaseTransfer
+from xphase_data import XphaseTransforms
 
 jpeg = TurboJPEG()
 
 ap = argparse.ArgumentParser()
 ap.add_argument('-i', '--input', required=True,
     help='path to input file')
+
+ap.add_argument('-m', '--mode', required=True, type=int, default=0,
+    help='analysis mode, read source code for details')
 
 args = vars(ap.parse_args())
 ap_name = ap.prog #TODO: Determine if we care about this, rawpy did it but we don't really need it.
@@ -66,19 +69,10 @@ else:
 if int(lensnum) not in [0, 1, 23, 24]:
     raw_data = np.fliplr(np.flipud(raw_data))
 
-xt = XphaseTransfer()
-lut = xt.lut
 
-print(np.amin(yplane))
-print(np.amax(yplane))
+xt = XphaseTransforms()
 
-print(raw_data.shape)
-print(rgbdata.shape)
 
-lindata = lut[rgbdata]
-print(np.amax(lindata))
-print(np.amax(raw_data))
-#raw_data = ilut_flor[raw_data.astype(np.uint16)]
 
 #DNG color matrix
 color_matrix_raw = np.matrix([[1.69266987, -0.5626429913, -0.08418130087],
@@ -89,30 +83,17 @@ primaries_raw = colour.primaries_whitepoint(invmatrix_raw)[0]
 whitepoint_raw = colour.primaries_whitepoint(invmatrix_raw)[1]
 colorspace_raw = colour.models.RGB_Colourspace('RAW color space', primaries_raw, whitepoint_raw, use_derived_matrix_RGB_to_XYZ=True, use_derived_matrix_XYZ_to_RGB=True)
 
-#Where the heck did I get this from???
-#Linearizing Zwikel's colorchecker shot and then dcamprofing it???
-#Or tracing the borders of a HALD-derived DNG that has no values below 23?
-primaries_jpeg = np.array([[0.567968, 0.368446],
-                           [0.30249, 0.64607],
-                           [0.1293, 0.006655]])
-whitepoint_jpeg = np.array([0.34567, 0.3585])
-colorspace_jpeg = colour.models.RGB_Colourspace('JPEG color space', primaries_jpeg, whitepoint_jpeg, use_derived_matrix_RGB_to_XYZ=True, use_derived_matrix_XYZ_to_RGB=True)
+# Estimated color matrix from from passing create_jpeg mode 2 with minval=23 and maxval=227 to rawhist mode 3
+jpeg_to_xyz = np.array([[ 0.55057961,  0.29936044,  0.10435369],
+       [ 0.35709506,  0.63883644,  0.00537558],
+       [ 0.06214492,  0.05033535,  0.69739864]])
+colorspace_jpeg = colour.models.RGB_Colourspace('JPEG color space', colour.models.primaries_whitepoint(jpeg_to_xyz)[0],
+                                                colour.models.primaries_whitepoint(jpeg_to_xyz)[1], matrix_RGB_to_XYZ=jpeg_to_xyz, matrix_XYZ_to_RGB=np.linalg.inv(jpeg_to_xyz))
 
 raw_to_jpeg_matrix = colour.matrix_RGB_to_RGB(colorspace_raw, colorspace_jpeg)
 
-# Estimated color matrix from an earlier mode 0 run
-cmat_est1 = np.array([[0.74066847, 0.20708179, 0.10163293],
-                    [0.14307383, 0.62007526, 0.15846967],
-                    [0.11811859, 0.19158811, 0.73919268]])
 
-# Sanity check our matrices
-pprint(raw_to_jpeg_matrix)
-pprint(np.linalg.inv(raw_to_jpeg_matrix))
-pprint(cmat_est1)
-
-jpeg_to_raw_matrix = np.linalg.inv(raw_to_jpeg_matrix)
-
-mode = 2
+mode = args['mode']
 match mode:
     case 0:
         # Attempt to reverse engineer the color matrix that transforms from JPEG to RAW by fitting a matrix to
@@ -120,7 +101,7 @@ match mode:
         # 23 to 229.  (create.jpeg.py mode 2 with minval=23 and maxval=229)
         # Trying to fit outside of the luma-only LUT region creates too many reverse engineering variables
         ldata = np.reshape(lindata, (lindata.shape[0]*lindata.shape[1], lindata.shape[2]))
-        print(ldata.shape)
+
         rdata = np.reshape(raw_data, (raw_data.shape[0]*raw_data.shape[1], raw_data.shape[2]))
         cmat = np.matmul(np.matmul(np.linalg.inv(np.matmul(ldata.T,ldata)), ldata.T), rdata)
 
@@ -188,7 +169,7 @@ match mode:
         #Intended for images generated using create_jpeg.py modes 0 and 1
         h = rgbdata.shape[0]
         w = rgbdata.shape[1]
-        mod_data = np.matmul(raw_data, raw_to_jpeg_matrix.T)
+        mod_data = np.matmul(raw_data, np.linalg.inv(cmat_est1).T)
         for i in range(3):
             plt.figure()
             rdslice = mod_data[0::8,int((w/3)*i + w/6), :]
@@ -202,12 +183,24 @@ match mode:
             plt2.plot(rgbdata[0::8,int((w/3)*i + w/6), 2], 'b')
 
             plt.figure()
+            x = np.linspace(0,255,4000)
+            plt.plot(x, xt.linearize_code(x), 'k')
+            plt.plot(np.arange(256), xt.lut[np.arange(256)], 'm')
             plt.plot(rgbdata[0::8,int((w/3)*i + w/6), 0], rdslice[:,0],'r')
             plt.plot(rgbdata[0::8,int((w/3)*i + w/6), 1], rdslice[:,1],'g')
             plt.plot(rgbdata[0::8,int((w/3)*i + w/6), 2], rdslice[:,2],'b')
-            x = np.linspace(0,255,2000)
-            plt.plot(x, xt.linearize_code(x), 'k')
-            
+            plt.axhline(y=0, color='k', alpha=0.5)
+
+
+            plt.figure()
+            plt.plot(rgbdata[0::8,int((w/3)*i + w/6), 0], rdslice[:,0]/xt.linearize_code(rgbdata[0::8,int((w/3)*i + w/6), 0]),'r')
+            plt.plot(rgbdata[0::8,int((w/3)*i + w/6), 1], rdslice[:,1]/xt.linearize_code(rgbdata[0::8,int((w/3)*i + w/6), 1]),'g')
+            plt.plot(rgbdata[0::8,int((w/3)*i + w/6), 2], rdslice[:,2]/xt.linearize_code(rgbdata[0::8,int((w/3)*i + w/6), 2]),'b')        
+
+            plt.figure()
+            plt.plot(rgbdata[0::8,int((w/3)*i + w/6), 0][:-1], np.diff(rdslice[:,0]),'r')
+            plt.plot(rgbdata[0::8,int((w/3)*i + w/6), 1][:-1], np.diff(rdslice[:,1]),'g')
+            plt.plot(rgbdata[0::8,int((w/3)*i + w/6), 2][:-1], np.diff(rdslice[:,2]),'b') 
 
         w6 = int(w/6)
         raw_data=raw_data[0::8,w6::2*w6,:]
@@ -217,8 +210,39 @@ match mode:
         raw_data = raw_data[lumidx]
         lum_data = lum_data[lumidx]
         print(lum_data.shape)
-        cplot = colour.plotting.plot_RGB_chromaticities_in_chromaticity_diagram_CIE1931(raw_data,colourspace=colorspace_raw,scatter_kwargs={'s':1,'c':lum_data},show=False)
+        cplot = colour.plotting.plot_RGB_chromaticities_in_chromaticity_diagram_CIE1931(raw_data,colourspace=colorspace_raw,scatter_kwargs={'s':5,'c':lum_data},show=False)
         cplot[0].colorbar(cplot[1].collections[2],ax=cplot[1])
         cplot = colour.plotting.plot_planckian_locus_in_chromaticity_diagram_CIE1931(['A','D50','D65'],axes=cplot[1],show=False)
         cplot = colour.plotting.plot_RGB_colourspaces_in_chromaticity_diagram_CIE1931([colorspace_raw, colorspace_jpeg, colour.models.RGB_COLOURSPACE_sRGB],axes=cplot[1],show=False)
         plt.show()
+
+    case 3:
+        #Compare image from PanoManager with transforms REd from Ghidra
+        h = rgbdata.shape[0]
+        w = rgbdata.shape[1]
+        rawdata_from_jpg = xt.jpeg_to_raw(rgbdata)
+
+        plt.imshow(raw_data/65281)
+        plt.figure()
+        plt.imshow(rawdata_from_jpg/65281)
+        plt.figure()
+        plt.imshow((raw_data/rawdata_from_jpg)/2.0)
+        plt.figure()
+        maxdelt = np.amax(raw_data-rawdata_from_jpg)
+        mindelt = np.amin(raw_data-rawdata_from_jpg)
+        print(maxdelt)
+        print(mindelt)
+        plt.imshow((raw_data-rawdata_from_jpg-mindelt)/(maxdelt-mindelt))
+        plt.show()
+        """        raw_data=raw_data[0::8,w6::2*w6,:]
+        raw_data = np.reshape(raw_data,(raw_data.shape[0]*raw_data.shape[1],raw_data.shape[2]))
+        lum_data = np.log2(np.amax(raw_data/65281.0,axis=1))
+        lumidx = np.argsort(lum_data)
+        raw_data = raw_data[lumidx]
+        lum_data = lum_data[lumidx]
+        print(lum_data.shape)
+        cplot = colour.plotting.plot_RGB_chromaticities_in_chromaticity_diagram_CIE1931(raw_data,colourspace=colorspace_raw,scatter_kwargs={'s':5,'c':lum_data},show=False)
+        cplot[0].colorbar(cplot[1].collections[2],ax=cplot[1])
+        cplot = colour.plotting.plot_planckian_locus_in_chromaticity_diagram_CIE1931(['A','D50','D65'],axes=cplot[1],show=False)
+        cplot = colour.plotting.plot_RGB_colourspaces_in_chromaticity_diagram_CIE1931([colorspace_raw, colorspace_jpeg, colour.models.RGB_COLOURSPACE_sRGB],axes=cplot[1],show=False)
+        plt.show() """
