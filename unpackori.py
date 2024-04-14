0ac94c9ca98b490c865a16782cb2515f70ffcd3f
Andy Dodd <atd7@cornell.edu>
2024-04-14 14:01:02 -0400

    unpackori: Remove long-obsolete if(0)ed and commented cruft

--- unpackori.py
+++ unpackori.py
@@ -48,11 +48,6 @@ bin_file = args['input']
 
 (filebase, ext) = bin_file.rsplit('.',1)
 
-#with open(bin_file,'rb') as myfile:
-#    for j in range(25):
-#        myfile.seek(0x434+3080*j)
-#        idx = myfile.read(2)
-#        print(hex(struct.unpack('<H',idx)[0]))
 
 with open(bin_file,'rb') as myfile:
     cur_offset = 2
@@ -165,35 +160,4 @@ for entrynum in range(int(tablelen/20)):
             print("Unknown image type " + str(imgtype) + " found in ORI table in entry " + str(entrynum) + ", aborting")
             exit(-1)
 
-#FIXME:  We are currently ignoring the last image in the ORI.  It's probably SOME sort of preview.
-#We should append the file size as the last entry in the list of occurances so the code below can handle it
-if(0):
-    for j in range(len(occurances)-1):
-        tbloffset = (occurances[j]-6)-imgstart
-        tblloc = oritable.find(struct.pack('<L', tbloffset))
-        datalen = occurances[j+1] - occurances[j]
-        print(str(j) + "," + str(tbloffset) + "," + str(tblloc) + "," + str(datalen))
-        if(mode == 0): #3-shot ORIs store images for a single lens together in bracket sequence
-            if(j % 2 == 0):  #Even numbered images are thumbnails, skip them
-                continue
-            dest_fname = str(lensidx) + "_" + str(bktidx) + ".jpg"
-            bktidx += 1
-            if(bktidx >= maxbkt):
-                lensidx += 1
-                bktidx = 0
-        elif(mode == 1):
-            #As opposed to 3-shot, 6-shot ORIs store all images for a given EV together,
-            #sequencing across lenses and then repeating
-            if(j % 2 == 0):  #Even numbered images are still thumbnails, skip them
-                continue
-            dest_fname = str(lensidx) + "_" + str(bktidx) + ".jpg"
-            #honestly we can keep the above common to both mode0 and mode1 and have the if here,
-            #FIXME later
-            lensidx += 1
-            if(lensidx >= maxlens):
-                bktidx += 1
-                lensidx = 0
-        else:
-            dest_fname = str(int(j)) + ".jpg"
-        #JFIF text is 6 bytes past beginning of file.  FIXME:  Handle this more cleanly
-        copypart(bin_file,dest_fname, occurances[j]-6, datalen)
+
